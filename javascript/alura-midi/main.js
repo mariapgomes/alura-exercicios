dd43290ad394feab7bdfdf01280ac1e79a70e4c0
Maria Gomes <mariapg.dev@gmail.com>
2023-06-28 17:13:37 -0300

    Cria a função de reproduzir som ao clicar ou selecionar as teclas da interface

--- javascript/alura-midi/main.js
+++ javascript/alura-midi/main.js
@@ -1,17 +1,35 @@
-// Da play no som ao clicar nos botoẽs
-const teclas = document.querySelectorAll('.tecla');
+// *Da play no som ao clicar nos botoẽs
 
-function btnReproduzir (event) {
-  const teclaClicada = event.currentTarget;
-  const nomeDaTecla = teclaClicada.innerText.toLowerCase();
-  const audio = document.querySelectorAll('audio');
-  audio.forEach((audio) => {
-    if (audio.classList.contains(nomeDaTecla)) {
-      audio.play()
+function reproduzSom () {
+  
+  function tocaSom(idAudio) {
+    const audio = document.querySelector(idAudio);
+  
+    if (audio && audio.localName === 'audio') {
+      audio.play();
+    } else {
+      console.log('Elemento não encontrado ou seletor inválido');
     }
+  }
+  
+  const teclas = document.querySelectorAll('.tecla');
+  
+  teclas.forEach((tecla) => {
+    const som = tecla.classList[1];
+    const idAudio = `#som_${som}`;
+    
+    tecla.addEventListener('keydown', (event) => {
+      if (event.code === 'Space' || event.code === 'Enter') {
+        tecla.classList.add('ativa');
+      }
+    });
+    
+    tecla.addEventListener('keyup', () => tecla.classList.remove('ativa'));
+  
+    tecla.addEventListener('click', () => {
+      tocaSom(idAudio);
+    });
   });
 }
 
-teclas.forEach((tecla) => {
-  tecla.addEventListener('click', btnReproduzir);
-})
+reproduzSom ()
