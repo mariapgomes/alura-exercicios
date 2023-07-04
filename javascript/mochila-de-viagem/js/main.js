944193e4775c03587207ef6dd37012b7f5b9036e
Maria Gomes <mariapg.dev@gmail.com>
2023-07-04 15:54:03 -0300

    Cria a função de salvar os itens da lista no localStorage e exista atualiza na página

--- javascript/mochila-de-viagem/js/main.js
+++ javascript/mochila-de-viagem/js/main.js
@@ -1,22 +1,54 @@
 const form = document.querySelector('#novoItem');
 const lista = document.querySelector('#lista');
+const itens = JSON.parse(localStorage.getItem('itens')) || [];//JSON.parse() converte um array que foi convertido em uma string anteriormente de volta em um array.
 
-function criaElemento(nome, quantidade) {
+function criaElemento(item) {
   const novoItem = document.createElement('li');
   novoItem.classList.add('item');
 
   const numeroItem = document.createElement('strong');
-  numeroItem.innerHTML = quantidade;
+  numeroItem.dataset.id = item.id;
+  numeroItem.innerHTML = item.quantidade;
+
   
   novoItem.appendChild(numeroItem);
-  novoItem.innerHTML += nome;
+  novoItem.innerHTML += item.nome;
 
   lista.appendChild(novoItem);
 }
 
+itens.forEach((elemento) => {
+  criaElemento(elemento);
+});
+
+function atualizaElemento(item) {
+  const idDoItem = document.querySelector('[data-id="'+item.id+'"]');
+  idDoItem.innerText = item.quantidade;
+
+}
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const nome = event.target.elements['nome'].value;
-  const quantidade = event.target.elements['quantidade'].value;
-  criaElemento(nome, quantidade);
+
+  const nome = event.target.elements['nome'];
+  const quantidade = event.target.elements['quantidade'];
+  const existe = itens.find((elemento) => elemento.nome === nome.value);//array.find(parâmetro), verifica se a condição dentro dele existe no array definido.
+  const itemAtual = {
+    nome: nome.value,
+    quantidade: quantidade.value
+  };
+  
+  if(existe) {
+    itemAtual.id = existe.id;
+    atualizaElemento(itemAtual);
+  } else {
+    itemAtual.id = itens.length
+    criaElemento(itemAtual);
+    itens.push(itemAtual);
+  }
+
+  localStorage.setItem('itens', JSON.stringify(itens)); //JSON.stringify(itens) transforma um objeto em uma string.
+
+  nome.value = '';
+  quantidade.value = '';
 });
