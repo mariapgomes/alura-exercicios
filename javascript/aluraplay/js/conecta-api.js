c338499c33f2a3409cad3e4527248a657d733615
Maria Gomes <mariapg.dev@gmail.com>
2023-08-28 13:14:52 -0300

    Faz a conexão com a API

---
+++ javascript/aluraplay/js/conecta-api.js
@@ -0,0 +1,5 @@
+export async function listaVideos() {
+  const conexao = await fetch ('http://localhost:3000/videos');
+  const conexaoConvertida = await conexao.json();
+  return conexaoConvertida;
+}
