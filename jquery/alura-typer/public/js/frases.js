9262827558a321fe3974032ed49d8f41f341e561
Maria Gomes <mariapg.dev@gmail.com>
2023-09-19 14:53:14 -0300

    Busca frase a partir do ID

--- jquery/alura-typer/public/js/frases.js
+++ jquery/alura-typer/public/js/frases.js
@@ -1,4 +1,5 @@
 $('#botao-frase').click(fraseAleatoria);
+$('#botao-frase-id').click(buscaFrase);
 let $frase = $('.frase');
 
 function fraseAleatoria() {
@@ -17,10 +18,35 @@ function fraseAleatoria() {
 
 function trocaFrase(dados) {
   let numeroAleatorio = Math.floor(Math.random() * dados.length),
-      $tempoFrase = dados[numeroAleatorio].tempo;
+      tempoFrase = dados[numeroAleatorio].tempo;
   
   $frase.text(dados[numeroAleatorio].texto);
 
   atualizaTamanhoFrase();
-  atualizaTempoInicial($tempoFrase);
+  atualizaTempoInicial(tempoFrase);
+}
+
+function buscaFrase() {
+  $('#spinner').show();
+  const $fraseId = $('#frase-id').val(),
+        $buscaFraseId = {id: $fraseId};
+
+  $.get('http://localhost:3000/frases', $buscaFraseId, exibeFrase)
+  .fail(function() {
+    $('#erro').show();
+    setTimeout(function() {
+      $('#erro').hide();
+    },3000);
+  })
+  .always(function() {
+    $('#spinner').hide();
+  });
+}
+
+function exibeFrase(dados) {
+  let tempoFrase = dados.tempo;
+  $frase.text(dados.texto);
+
+  atualizaTamanhoFrase();
+  atualizaTempoInicial(tempoFrase);
 }
